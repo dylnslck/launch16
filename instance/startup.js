78492ccb109c63a05f5819324b7a6936238a21bb
Dylan <dylanslack@gmail.com>
2016-02-27 13:25:00 -0800

    Create npm installer and node runner

--- instance/startup.js
+++ instance/startup.js
@@ -1,9 +1,33 @@
-const rp = require('request-promise');
+'use strict';
+
 const Firebase = require('firebase');
+const fs = require('fs-extra');
+const path = require('path');
+const proc = require('child_process');
+const appId = process.env.APP_ID || '-KBYPhy_Q7EtKQC74CuH';
+const appRef = new Firebase(`https://restle-launch2016.firebaseio.com/apps/${appId}`);
 
-const userId = process.env.USER_ID;
-const f = new Firebase('https://restle-launch2016.firebaseio.com/users');
+const build = image => {
+  Object.keys(image).forEach(key => {
+    const name = new Buffer(key, 'base64').toString('ascii');
+    const file = new Buffer(image[key], 'base64').toString('ascii');
+    fs.outputFileSync(path.resolve(__dirname, 'dist', name), file);
+  });
+};
 
-f.once('value', snapshot => {
-  console.log(snapshot.val());
+let child;
+
+// when the app image changes, reinstantiate the restle app
+appRef.child('image').on('value', snapshot => {
+  build(snapshot.val());
+
+  if (child && child.connected) {
+    child.disconnect();
+  }
+
+  proc.execSync('cd dist/app && npm install');
+  child = proc.spawn('node', [path.resolve(__dirname, 'dist/app', 'index.js')]);
+
+  child.stdout.on('data', data => process.stdout.write(data));
+  child.stderr.on('data', data => process.stderr.write(data));
 });
