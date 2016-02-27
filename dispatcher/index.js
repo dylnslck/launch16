0208a1a472579c4c6312f3b9f8ac16c39ab19b78
Dylan <dylanslack@gmail.com>
2016-02-27 13:25:28 -0800

    Add instatiate route

--- dispatcher/index.js
+++ dispatcher/index.js
@@ -1,13 +1,53 @@
+'use strict';
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const AWS = require('aws-sdk');
+
+const port = process.env.PORT || 3000;
 const app = express();
+
+AWS.config.region = 'us-east-1';
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res, next) => {
-  return res.json({ hello: 'world' });
+app.post('/instantiate', (req, res, next) => {
+  const ec2 = new AWS.EC2();
+  const name = req.body.name || 'random-name';
+  const user = req.body.user || '12345';
+  const appId = req.body.app || 'app';
+
+  let params = {
+    ImageId: 'ami-e29ba788', // restle-image-1
+    InstanceType: 't2.micro',
+    MinCount: 1, MaxCount: 1,
+  };
+
+  // Create the instance
+  ec2.runInstances(params, (err, data) => {
+    if (err) { console.log("Could not create instance", err); return; }
+
+    const instanceId = data.Instances[0].InstanceId;
+
+    // Add tags to the instance
+    params = {
+      Resources: [instanceId],
+      Tags: [{
+        Key: 'Name', Value: name,
+      }, {
+        Key: 'UserId', Value: user,
+      }, {
+        Key: 'AppId', Value: appId,
+      }],
+    };
+
+    ec2.createTags(params, tagErr => {
+      console.log("Tagging instance", tagErr ? "failure" : "success");
+    });
+  });
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(port);
+console.log(`Listening on port ${port}.`);
