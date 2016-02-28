4878b0a391a7f315126a2cfce0ec717604426cdb
Dylan <dylanslack@gmail.com>
2016-02-27 23:34:32 -0800

    Add deploy status when deploying

--- instance/startup.js
+++ instance/startup.js
@@ -12,6 +12,7 @@ const build = image => {
   Object.keys(image).forEach(key => {
     const name = new Buffer(key, 'base64').toString('ascii');
     const file = new Buffer(image[key], 'base64').toString('ascii');
+
     fs.outputFileSync(path.resolve(__dirname, 'dist', name), file);
   });
 };
@@ -23,11 +24,16 @@ rp({ json: true, uri: metaDataUrl }).then(content => {
   const instanceId = content.instanceId;
   const getApp = `aws ec2 describe-tags --filters "Name=resource-id,Values=${instanceId}" "Name=key,Values=AppId" --region=us-east-1 --output=text | cut -f5`;
   const getUser = `aws ec2 describe-tags --filters "Name=resource-id,Values=${instanceId}" "Name=key,Values=UserId" --region=us-east-1 --output=text | cut -f5`;
+
+  // direct port 80 to 8000
   proc.execSync('sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8000');
+
+  // retrieve app and user EC2 metadata
   const appId = proc.execSync(getApp, { encoding: 'utf-8' }).replace('\n', '');
   const userId = proc.execSync(getUser, { encoding: 'utf-8' }).replace('\n', '');
 
-  appRef = new Firebase('https://restle-launch2016.firebaseio.com/apps/' + appId);
+  // create the Firebase ref
+  appRef = new Firebase(`https://restle-launch2016.firebaseio.com/apps/${appId}`);
 
   process.env.USER_ID = userId;
   process.env.APP_ID = appId;
@@ -35,15 +41,20 @@ rp({ json: true, uri: metaDataUrl }).then(content => {
 }).then(() => {
   // when the app image changes, reinstantiate the restle app
   appRef.child('image').on('value', snapshot => {
+    const appName = appRef.key('name');
+    appRef.child('isDeploying').set(true);
+
     build(snapshot.val());
 
+    // if the instance is already running, restart it
     if (child && child.connected) {
       child.disconnect();
     }
 
     proc.execSync('cd ~/launch16/instance/dist/app && npm install');
-    child = proc.spawn('node', [path.resolve(__dirname, 'dist/app', 'index.js')]);
+    child = proc.spawn('node', [path.resolve(__dirname, `dist/${appName}`, 'index.js')]);
 
+    appRef.child('isDeploying').set(false);
     child.stdout.on('data', data => process.stdout.write(data));
     child.stderr.on('data', data => process.stderr.write(data));
   });
